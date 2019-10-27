31526dd4881bda1313a27e0220a83bfda5188ef3
Yann David <davidyann88@gmail.com>
2019-10-26 20:07:23 -0400

    Working writes

--- lua/ln.lua
+++ lua/ln.lua
@@ -9,8 +9,35 @@ local path = ngx.var.request_uri
 local realPath = path
 local httpc = http.new()
 
+ngx.req.read_body()
 headers = ngx.req.get_headers()
 body = ngx.req.get_body_data()
+
+if not (ngx.req.get_method() == "GET") then
+  ngx.log(ngx.INFO, "Not a GET request, passing to " .. vaultURL .. path .. " as is.")
+
+  local res, err = httpc:request_uri(vaultURL .. path, {
+    method = ngx.req.get_method(),
+    body = body,
+    headers = headers,
+    ssl_verify = true
+  })
+
+  if err ~= nil then
+    ngx.status = 503
+    ngx.say(err)
+    ngx.flush(true)
+    return
+  end
+
+  ngx.status = res.status
+
+  for h, v in pairs(res.headers) do
+    ngx.header[h] = v
+  end
+
+  return ngx.print(res.body)
+end
 
 if not string.find(path, "/v1/secret") then
   ngx.log(ngx.INFO, "Not a secret, passing to " .. vaultURL .. path .. " as is.")
@@ -45,8 +72,10 @@ if symlinksPath == nil or symlinksPath == "" then
   symlinksPath = "symlinks"
 end
 
+ngx.log(ngx.ERR, vaultURL .. "/v1/secret/data/" .. symlinksPath)
+
 local res, err = httpc:request_uri(vaultURL .. "/v1/secret/data/" .. symlinksPath, {
-  method = ngx.req.get_method(),
+  method = "GET",
   body = body,
   headers = headers,
   ssl_verify = true
@@ -59,11 +88,29 @@ if err ~= nil then
   return
 end
 
+ngx.log(ngx.ERR, res.status)
+
 if res.status == 404 then
+  local res, err = httpc:request_uri(vaultURL .. path, {
+    method = ngx.req.get_method(),
+    body = body,
+    headers = headers,
+    ssl_verify = true
+  })
+
+  if err ~= nil then
+    ngx.status = 503
+    ngx.say(err)
+    ngx.flush(true)
+    return
+  end
+
   ngx.status = res.status
+
   for h, v in pairs(res.headers) do
     ngx.header[h] = v
   end
+
   return ngx.print(res.body)
 end
 
